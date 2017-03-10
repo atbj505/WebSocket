e4670a79f78fe8d292e14841f9152a9155694c23
atbj505 <atbj505@hotmail.com>
2017-03-10 11:23:46 +0800

    websocket init

--- webSocketServer.py
+++ webSocketServer.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import tornado.web
+import tornado.ioloop
+
+
+class Index(tornado.web.RequestHandler):
+    def get(self):
+        self.write('<html><body>Hello, world!')
+
+
+if __name__ == '__main__':
+    app = tornado.web.Application([
+        ('/', Index),
+    ])
+    app.listen(8000)
+    tornado.ioloop.IOLoop.instance().start()
