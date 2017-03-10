3009ad932810ca3e3036dc5f341e84b6761d4f93
atbj505 <atbj505@hotmail.com>
2017-03-10 14:05:52 +0800

    demo websocket

--- webSocketServer.py
+++ webSocketServer.py
@@ -3,16 +3,26 @@
 
 import tornado.web
 import tornado.ioloop
+import tornado.websocket
 
 
 class Index(tornado.web.RequestHandler):
     def get(self):
-        self.write('<html><body>Hello, world!')
+        self.render('templates/index.html')
+
+
+class SocketHandler(tornado.websocket.WebSocketHandler):
+    clients = set()
+
+    # @staticmethod
+    def open(self):
+        self.write_message('Welcome to WebSocket')
 
 
 if __name__ == '__main__':
     app = tornado.web.Application([
         ('/', Index),
+        ('/soc', SocketHandler),
     ])
     app.listen(8000)
     tornado.ioloop.IOLoop.instance().start()
